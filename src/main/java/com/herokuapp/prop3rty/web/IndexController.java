23699fe358fa5a24db4a8c2951845fc9fa66fffd
Căin CRISTEA <calin@cristeanet.com>
2016-01-24 21:51:14 +0200

    home page tryouts

--- src/main/java/com/herokuapp/prop3rty/web/IndexController.java
+++ src/main/java/com/herokuapp/prop3rty/web/IndexController.java
@@ -2,12 +2,15 @@ package com.herokuapp.prop3rty.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-	@RequestMapping("/")
-	String index() {
-		return "index";
+	@RequestMapping("")
+	public ModelAndView index() {
+		ModelAndView view = new ModelAndView("index");
+		return view;
 	}
 }
