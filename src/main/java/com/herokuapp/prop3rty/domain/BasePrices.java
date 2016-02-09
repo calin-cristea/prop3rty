377b5920a073e4288e3816b6f3a1e19ffebb901d
Călin CRISTEA <calin@cristeanet.com>
2016-02-09 18:33:51 +0200

    some cleaning

--- src/main/java/com/herokuapp/prop3rty/domain/BasePrices.java
+++
@@ -1,9 +0,0 @@
-package com.herokuapp.prop3rty.domain;
-
-public class BasePrices extends AbstractModel {
-	
-	private String neighborhood;
-	private int numbersOfRooms;
-	private double price;
-
-}
--- src/main/java/com/herokuapp/prop3rty/service/ApartmentsService.java
+++
@@ -1,5 +0,0 @@
-package com.herokuapp.prop3rty.service;
-
-public class ApartmentsService {
-
-}
