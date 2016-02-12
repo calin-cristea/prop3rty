adee82bf2906387718c5d8a03d9bd73c0452fc20
Rus David <davidrus.art@gmail.com>
2016-02-12 10:56:47 +0200

    work in progress......

---
+++ src/main/java/com/herokuapp/prop3rty/domain/Gas.java
@@ -0,0 +1,16 @@
+package com.herokuapp.prop3rty.domain;
+
+public enum Gas {
+	YES (0.00),
+	NO (-0.05);
+	
+	double value;
+	
+	Gas(double value) {
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+}
---
+++ src/main/java/com/herokuapp/prop3rty/domain/Land.java
@@ -0,0 +1,6 @@
+package com.herokuapp.prop3rty.domain;
+
+public class Land extends AssetModel{
+	int front;
+
+}
